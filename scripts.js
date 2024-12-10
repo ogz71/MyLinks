d8bd0706b0310d50e5f7e5319af5cc0b07d8183f
Oğuz Kaan <58365240+ogz71@users.noreply.github.com>
2024-12-10 15:12:40 +0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.querySelector('.loader').style.display = 'none';
-        document.querySelector('.container').classList.remove('hidden');
-    }, 3000); // 3 saniye sonra loader kaybolacak ve içerik görünecek
-});
